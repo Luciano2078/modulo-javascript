e5aa61a65f674654b89b6b8334060ea2de78af8f
Luciano Pereira <luciano.pereiraaristimunho@gmail.com>
2022-04-02 13:29:52 -0300

    new-update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-function calcularMedia(notas) {
+function calcularMedia( notas ) {
     let soma = 0;
-    for ( c = 0; c < notas.lenght; c++) {
+    for ( c = 0; c < notas.length; c++) {
         soma += notas[c];
     }
 
-    media = soma / notas.lenght;
+    media = soma / notas.length;
     return media;
 
 }
@@ -17,31 +17,140 @@ function aprovacao( notas ) {
     return 'Média: ' + media + ' - Resultado: ' + condicao;
 }
 
+/* Formulário de envio de dados para o calculo da média */
 
+const formulario1 = document.getElementById('formulario-01')
 
-document.addEventListener('submit', function( evento ) {
+if(formulario1)
+formulario1.addEventListener('submit', function( evento ) {
     
     evento.preventDefault();
     evento.stopPropagation();
 
-    let formulario = document.getElementById('formulario-01');
+    if( this.getAttribute('class').match(/erro/)) {
+        return false
+    }
 
-    let dados = new FormData(formulario);
-
-    let objeto = {};
+    let dados = new FormData(this);
 
     let notas = [];
 
     for(let key of dados.keys()) {
-        objeto[key] = dados.get(key);
 
-        notas.push( parseInt(dados.get(key)));
+        let numero = dados.get(key).match(/\d*/) ? Number(dados.get(key)) : 0;
+
+        if(!isNaN(numero)) {
+            notas.push(numero);
+        }
     }
     console.log(notas);
-    console.log(objeto);
 
     texto = aprovacao(notas)
 
     document.getElementById('resultado').innerHTML = texto;
 
 });
+    
+
+function validaCampo(elemento){
+
+    elemento.addEventListener('focusout', function(event) {
+        
+        event.preventDefault();
+    
+        if(this.value == ''){
+            document.querySelector('.mensagem').innerHTML = 'Verifique o preenchimento dos campos em vermelho';
+            this.classList.add('erro');
+            this.parentNode.classList.add('erro');
+            return false;
+        } else {
+            document.querySelector('.mensagem').innerHTML = '';
+            this.classList.remove('erro');
+            this.parentNode.classList.remove('erro');
+        }
+    });
+}
+
+function validaCampoNumerico(elemento){
+
+    elemento.addEventListener('focusout', function(event) {
+        
+        event.preventDefault();
+
+        let numero = this.value.match(/^[\d]5-[\d]3/) ? this.value.replace(/-/, "") : this.value;
+    
+        if(numero != "" && numero.match(/[0-9]*/) && numero >= 0 && numero <= 10) {
+            document.querySelector('.mensagem').innerHTML = '';
+            this.classList.remove('erro');
+            this.parentNode.classList.remove('erro');
+        } else {
+            document.querySelector('.mensagem').innerHTML = 'Verifique o preenchimento dos campos em vermelho';
+            this.classList.add('erro');
+            this.parentNode.classList.add('erro');
+            return false;
+        }
+    });
+}
+
+function validaEmail(elemento) {
+    elemento.addEventListener('focusout' , function(event) {
+
+        event.preventDefault();
+
+        const emailValido = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.[a-z]?$/i;
+
+        if(this.value.match(emailValido)) {
+            document.querySelector('.mensagem').innerHTML = "";
+            this.classList.remove('erro');
+            this.parentNode.classList.remove('erro');
+        } else {
+            document.querySelector('.mensagem').innerHTML = 'Verifique o preenchimento dos campos em destaque';
+            this.classList.add('erro');
+            this.parentNode.classList.add('erro');
+            return false;
+        }
+    });
+}
+
+function validaEstado(elemento) {
+    elemento.addEventListener('focusout' , function(event) {
+
+        event.preventDefault();
+
+        const validaUf = /^[a-z]?/i;
+
+        if(this.value.match(validaUf)) {
+            document.querySelector('.mensagem').innerHTML = "";
+            this.classList.remove('erro');
+            this.parentNode.classList.remove('erro');
+        } else {
+            document.querySelector('.mensagem').innerHTML = 'Verifique o preenchimento dos campos em destaque';
+            this.classList.add('erro');
+            this.parentNode.classList.add('erro');
+            return false;
+        }
+    });
+}
+
+let camposObrigatorios = document.querySelectorAll('input.obrigatorio');
+let camposNumericos = document.querySelectorAll('input.numero');
+let camposEmail = document.querySelectorAll('input.email');
+let camposUf = document.querySelectorAll('input.uf');
+
+for( let emFoco of camposObrigatorios) {
+    validaCampo(emFoco);
+}
+
+for( let emFoco of camposNumericos) {
+    validaCampoNumerico(emFoco);
+}
+
+for( let emFoco of camposEmail) {
+    validaEmail(emFoco);
+}
+
+for( let emFoco of camposUf) {
+    validaEstado(emFoco);
+}
+
+
